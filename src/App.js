8fb8de68be9eb1f5b081017b0abbb800eda67c0c
Nicolas Prothero <nprothero0917@gmail.comgit config --global user.name Nicolas Prothero>
2024-02-28 16:16:51 -0500

    first init, electronJS working including react, created HOWTORUN.txt file with neccessary terminal commands

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Productivity Pro App</h1>
+      <p>Hopefully this is running on your end</p>
+      <p>-nic</p>
+    </div>
+  );
+}
+
+export default App;
